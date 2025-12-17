b246765af74540785a4d7b150b9a5dcd164464db
henrymops89 <henry.mops89@gmail.com>
2025-12-17 21:16:26 +0100

    create fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,30 +2,34 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'HenryMods'
-description 'HM Dairy System - Multi-Framework Cow Milking System'
-version '1.0.0'
-
-shared_scripts {
-    '@ox_lib/init.lua',
-    'config.lua'
-}
-
-client_scripts {
-    'test.lua',
-    'bridge/framework.lua',
-    'bridge/inventory.lua',
-    'client/*.lua'
-}
-
-server_scripts {
-    'bridge/framework.lua',
-    'bridge/inventory.lua',
-    'server/*.lua'
-}
+author 'HM Scripts'
+description 'HM Dairy UI - Single Cow Mode mit Auto-Spawning'
+version '4.0.0'
 
 dependencies {
     'ox_lib',
     'ox_target',
     'ox_inventory'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'                -- Konfiguration
+}
+
+client_scripts {
+    'client/blip.lua',          -- Map-Blip
+    'client/cows.lua',          -- Kuh-Spawning
+    'client/ui.lua',            -- UI Management
+    'client/main.lua'           -- Event Handler
+}
+
+server_scripts {
+    'server/ui_integration.lua' -- Server Logic
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html'
 }
