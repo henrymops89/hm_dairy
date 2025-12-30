6c6e281ee7a9e0a1efc99d47049e25673f622910
henrymops89 <henry.mops89@gmail.com>
2025-12-30 04:45:53 +0100

    Update fxmanifest
    Update Version Number too 5.0.0
    Remove dependencies (ox_inventory for more inventory support)
    Add New server_scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,18 +3,17 @@ game 'gta5'
 lua54 'yes'
 
 author 'HM Scripts'
-description 'HM Dairy UI - Single Cow Mode mit Auto-Spawning'
-version '4.0.0'
+description 'HM Dairy - SECURE VERSION mit tgiann-inventory Support'
+version '5.0.0'
 
 dependencies {
     'ox_lib',
-    'ox_target',
-    'ox_inventory'
+    'ox_target'
 }
 
 shared_scripts {
     '@ox_lib/init.lua',
-    'config.lua'                -- Konfiguration
+    'config.lua'
 }
 
 client_scripts {
@@ -25,6 +24,9 @@ client_scripts {
 }
 
 server_scripts {
+    'sv_config.lua',            -- Server-Only Config (Discord Webhooks, etc.)
+    'bridge/inventory.lua',     -- Universal Inventory Bridge
+    'server/security.lua',      -- Security System
     'server/ui_integration.lua' -- Server Logic
 }
 
